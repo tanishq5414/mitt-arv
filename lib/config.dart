d48347b11dc471a3313b0447cf56f2425f2f4f00
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2023-08-18 18:54:14 +0530

    added config.dart

---
+++ lib/config.dart
@@ -0,0 +1,9 @@
+var tmdbAPIURL = "https://api.themoviedb.org/3";
+var tmdbsAPIKey = 'aaca67275eb3cadbeb3d62dc21e7182d';
+var tmdbacessToken =
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYWNhNjcyNzVlYjNjYWRiZWIzZDYyZGMyMWU3MTgyZCIsInN1YiI6IjY0ZDdjZTlhZDEwMGI2MDBmZjBhZmYxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gaMHqACzop_rffBArOWqoHcBv12TnRM_Hl5Fova8fUI';
+var posterTmdbImageUrl = "https://image.tmdb.org/t/p/w300";
+var backdropTmdbImageUrl = "https://image.tmdb.org/t/p/w1280";
+var authAPIURL = "https://mitt-arv.vercel.app/api";
+// var authAPIURL = "http://10.0.2.2:8080/api";
+ 
